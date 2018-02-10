84a6d10c90d2f03a8f21c54d49b89908252a9cf0
aeris <aerisdoescodes@gmail.com>
2018-02-10 08:57:32 +0800

    Update#4 Invite command added

--- src/commands/invite.js
+++ src/commands/invite.js
@@ -1,11 +1,12 @@
-// exports.run = function (client, msg, args) {
+exports.run = function (client, msg, args) {
 
-//     msg.channel.createMessage({ });
+    msg.channel.createMessage("Invite Bot: <https://discordapp.com/oauth2/authorize/?permissions=19949578&scope=bot&client_id=411538973664608257>\n" + 
+    "Support: https://discord.gg/wp2Z6yy");
 
-// };
+};
 
-// exports.usage = {
-//     main: '{prefix}{command}',
-//     args: '',
-//     description: 'Shows server and bot invite links'
-// };
+exports.usage = {
+    main: '{prefix}{command}',
+    args: '',
+    description: 'Shows server and bot invite links'
+};
