196327aa79278d5d2937460501a841d431e79918
aeris <aerisdoescodes@gmail.com>
2018-03-01 19:16:39 +0800

    Update #40: Just small fix

--- src/commands/about.js
+++ src/commands/about.js
@@ -2,14 +2,14 @@ exports.run = function (client, msg) {
 
     if(!msg.channel.permissionsOf(client.user.id).has('embedLinks')) return msg.channel.createMessage("I don't have `Send Embed` permission.\nPlease contact an administrator if you think this is a bug.");
 
-    
+
     msg.channel.createMessage({embed:{
         color: config.options.embedColour,
         title: 'Cookie 🍪',
         description: ("Coded by aeris#0018 with love of cookies.\n\n") +
-        ("I'm a multi-purpose discord bot that does fun and useful things coded in NodeJS and the library used is [Eris](http://npmjs.com/package/eris). Why Eris? Eris is fast, lightweight, consistent,") + 
+        ("I'm a well coded discord bot that does NSFW, Images, fun and useful commands coded in NodeJS and the library used is [Eris](http://npmjs.com/package/eris). Why Eris? Eris is fast, lightweight, consistent,") +
         ("predictable, flexible and up-to-date.\n\n") +
-        ("If you like me please add me to your server!\nhttps://discordapp.com/oauth2/authorize/?permissions=19949578&scope=bot&client_id=411538973664608257\n\n") +
+        ("If you like me please add me to your server!\n[discord.invite.bot](https://discordapp.com/oauth2/authorize/?permissions=19949578&scope=bot&client_id=411538973664608257)\n\n") +
         ("Join my server for free cookies!\nhttps://discord.gg/DP9949T")
     }})
 }
