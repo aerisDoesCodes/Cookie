16a51aed7b1e8c3545680e5fe8a25075438be479
aeris <aerisdoescodes@gmail.com>
2018-03-03 07:12:00 +0800

    Update #48: ok, dev command only

--- src/commands/postdb.js
+++ src/commands/postdb.js
@@ -1,6 +1,7 @@
 const superagent = require('superagent')
 
 exports.run = function (client, msg) {
+  if (msg.author.id !== '241216483592634368') return;
   superagent.post(`https://discordbots.org/api/bots/stats`)
     .set('Authorization', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjQxMTUzODk3MzY2NDYwODI1NyIsImJvdCI6dHJ1ZSwiaWF0IjoxNTE5OTIyNDYyfQ.ZC7kyDkP6f7xBeC0sEn0kB9w8V7CrgG8THD9OvsU_To')
     .send({ server_count: client.guilds && client.guilds.size ? client.guilds.size : (client.Guilds ? client.Guilds.size : Object.keys(client.Servers).length) })
